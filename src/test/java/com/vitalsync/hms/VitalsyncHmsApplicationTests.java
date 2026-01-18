50fc7a0b47b548a4657337c3fa6526d1b7e9ee18
Tuhin Kumar Nandi <rijustar1998@gmail.com>
2026-01-18 22:22:02 +0530

    initial setup

---
+++ src/test/java/com/vitalsync/hms/VitalsyncHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalsync.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalsyncHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
